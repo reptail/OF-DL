6db015315004e334f2c111bd4f622652fb08ac20
Casper Sparre <reptail@gmail.com>
2025-03-08 15:04:47 +0100

    Added earningId to Subscribe model

--- OF DL/Entities/Subscriptions.cs
+++ OF DL/Entities/Subscriptions.cs
@@ -98,6 +98,7 @@ namespace OF_DL.Entities
             public object id { get; set; }
             public int? userId { get; set; }
             public int? subscriberId { get; set; }
+            public long? earningId { get; set; }
             public DateTime? date { get; set; }
             public int? duration { get; set; }
             public DateTime? startDate { get; set; }
